3118a3c0b0646d3c5e3b20d578bf0ad73b975d83
Alexandre Segura <mex.zktk@gmail.com>
2018-11-12 11:51:46 +0100

    [TEMP] Revert changes.

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,19 +15,19 @@ if ($dockerBridgeIp = getenv('DOCKER_BRIDGE_IP')) {
     $whiteListedAddresses[] = $dockerBridgeIp;
 }
 
-// if (isset($_SERVER['HTTP_CLIENT_IP'])
-//     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-//     || !(in_array(@$_SERVER['REMOTE_ADDR'], $whiteListedAddresses) || php_sapi_name() === 'cli-server')
-// ) {
-//     header('HTTP/1.0 403 Forbidden');
-//     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-// }
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], $whiteListedAddresses) || php_sapi_name() === 'cli-server')
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
-$kernel = new AppKernel('test', true);
+$kernel = new AppKernel('dev', true);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
